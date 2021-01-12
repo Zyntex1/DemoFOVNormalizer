166252996b3538c024c29718adacd5506e2570c6
Zyntex <33593734+Zyntex1@users.noreply.github.com>
2021-01-12 21:17:05 +0100

    first commit

---
+++ DemoFOVNormalizer/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DemoFOVNormalizer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Normalize FOV!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 237);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Demo List";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(101, 209);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 22);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Deselect all";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(188, 184);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(254, 75);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Demo Folder";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(154, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 24);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Select Folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Portal 2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Demo FOV Normalizer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DemoFOVNormalizer/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoFOVNormalizer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        List<string> files = new List<string>();
+        List<string> selectedFiles = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                foreach (string path in selectedFiles) {
+                    byte[] content = File.ReadAllBytes(path);
+                    byte[] pattern = new byte[] { 0x63, 0x6C, 0x5F, 0x66, 0x6F, 0x76 };
+                    using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite)) {
+                        foreach (int pos in content.Locate(pattern)) {
+                            fs.Position = pos + 7;
+                            fs.WriteByte(0x39);
+                            fs.Position = pos + 8;
+                            fs.WriteByte(0x30);
+                            if (content[pos + 9] != 0x20) {
+                                fs.Position = pos + 9;
+                                fs.WriteByte(0x00);
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++) {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++) {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            try {
+                folderBrowserDialog1.ShowDialog();
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            } catch (Exception ex) { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            try {
+                checkedListBox1.Items.Clear();
+                files.Clear();
+                foreach (string file in Directory.GetFiles(textBox1.Text)) {
+                    files.Add(file);
+                    checkedListBox1.Items.Add(Path.GetFileNameWithoutExtension(file));
+                }
+            } catch (Exception ex) { }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            try {
+                selectedFiles.Clear();
+                foreach (int index in checkedListBox1.CheckedIndices) {
+                    selectedFiles.Add(files[index]);
+                }
+            } catch (Exception ex) { }
+        }
+    }
+}
